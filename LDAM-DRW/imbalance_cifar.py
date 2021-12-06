de8bfcb7fa836526f29b349a6d03b6bc691dd4ff
Jinyi Tao <34171969+estella2012@users.noreply.github.com>
2021-12-06 14:49:41 -0500

    change symmetric noise data generation

--- LDAM-DRW/imbalance_cifar.py
+++ LDAM-DRW/imbalance_cifar.py
@@ -80,7 +80,7 @@ class NOMNIST(torchvision.datasets.MNIST):
         super(NOMNIST, self).__init__(root, download=download, transform=transform,
                                            target_transform=target_transform)
         if asym:
-            print('Asym')
+            print('Asym = ', asym)
             img_num_list = self.get_img_num_per_cls(self.cls_num, imb_type, imb_factor)
             self.gen_imbalanced_data(img_num_list)
             source_class = [7, 2, 3, 5, 6]
@@ -98,26 +98,27 @@ class NOMNIST(torchvision.datasets.MNIST):
             np.random.seed(0)
 
         elif nosiy_rate > 0:
+            print('Asym = ', asym)
             img_num_list = self.get_img_num_per_cls(self.cls_num, imb_type, imb_factor)
             self.gen_imbalanced_data(img_num_list)
+
             n_samples = len(self.targets)
             n_noisy = int(nosiy_rate * n_samples)
             print("%d Noisy samples" % (n_noisy))
             class_index = [np.where(np.array(self.targets) == i)[0] for i in range(10)]
-            class_noisy = int(n_noisy / 10)
             noisy_idx = []
             for d in range(10):
+                class_noisy = int(nosiy_rate * len(class_index[d]))
                 noisy_class_index = np.random.choice(class_index[d], class_noisy, replace=False)
                 noisy_idx.extend(noisy_class_index)
                 print("Class %d, number of noisy % d" % (d, len(noisy_class_index)))
             for i in noisy_idx:
                 self.targets[i] = torch.tensor(other_class(n_classes=10, current_class=self.targets[i]))
-            print(len(noisy_idx))
-            print("Print noisy label generation statistics:")
-            for i in range(10):
-                n_noisy = np.sum(np.array(self.targets) == i)
-                print("Noisy class %s, has %s samples." % (i, n_noisy))
-            np.random.seed(0)
+        print("Print noisy label generation statistics:")
+        for i in range(10):
+            n_noisy = np.sum(np.array(self.targets) == i)
+            print("Noisy class %s, has %s samples." % (i, n_noisy))
+        np.random.seed(0)
             
             
     def get_img_num_per_cls(self, cls_num, imb_type, imb_factor):
